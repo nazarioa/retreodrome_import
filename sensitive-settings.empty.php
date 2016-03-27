401dde8e085adc490e88340c9b9d4e220edf3629
Nazario A. Ayala <nazario@niztech.com>
2016-03-27 10:25:44 -0700

    Added an empty sensitive-settings file

---
+++ sensitive-settings.empty.php
@@ -0,0 +1,12 @@
+<?php
+<?php
+// Make a copy of this file and rename it to:
+// sensitive-settings.php
+$settings = array(
+  'database_type' => 'mysql',
+  'database_name' => 'name_here',
+  'server' => 'server_address_here',
+  'username' => 'user_name_here',
+  'password' => 'password_here',
+  'charset' => 'utf8'
+);
