249c8cafb180dfa5e5d54da320045ae8c7490d0b
Nazario A. Ayala <nazario@niztech.com>
2016-03-27 02:35:45 -0700

    General corrections

--- import-master.php
+++ import-master.php
@@ -17,7 +17,7 @@ foreach ($xml_data as $game) {
   // Create a Game entry, no need to check if this
   // game exists already since the database is empty.
   $last_game_id = $database->insert ('games', [
-    'description' => (string) $game->title,
+    'description' => (string) $game->cartridge->title,
     'disabled' => NO,
   ]);
   check_database_error($database);
@@ -35,7 +35,7 @@ foreach ($xml_data as $game) {
 
   // Associating "Cartridges" with current game.
   foreach ($game->cartridge as $cartridge) {
-    $title = $cartridge->title;
+    $title = (string) $cartridge->title;
     $region_type_id = get_region_id ($cartridge->region, $database, true);
     $game_default = ( !empty ($cartridge['primary']) && 'Y' == $cartridge['primary'] ) ?  YES : NO;
     $license = truthy ($cartridge->license);
@@ -58,53 +58,65 @@ foreach ($xml_data as $game) {
 
     // Associating Consoles
     // consoles are processed after cartridge insert
-    foreach ($consoles as $console) {
+    foreach ($cartridge->consoles as $console) {
       $console_id = get_console_id ($console->console, $database, true);
       $database->insert (TBL_CARTRIDGE_CONSOLE, [
         'console_id' => (int) $console_id,
         'cartridge_id' => (int) $last_cartridge_id,
       ]);
+      check_database_error($database);
     }
 
     // Associating Companies
     // companies are processed after cartridge insert
-    foreach ($companies as $company) {
-      $company_role_type_id = get_type ( (string) $company['type'], CREATION_ROLE, $database, true);
-      $company_id = get_company_id ($company->company, $database, true);
-        'console_id' => (int) $console_id,
+    foreach ($cartridge->companies as $company) {
+      $company_role_type_id = get_type ( (string) $company->company['type'], CREATION_ROLE, $database, true);
+      $company_id = get_company_id ( $company->company, $database, true);
       $database->insert (TBL_CARTRIDGE_COMPANY, [
+        'company_id' => (int) $company_id,
         'cartridge_id' => (int) $last_cartridge_id,
         'company_role_type_id' => (int) $company_role_type_id,
       ]);
+      check_database_error($database);
     }
 
     // Associating Releases
     // releases are processed after cartridge insert
-    foreach ($releases as $release) {
-      $country_type_id = get_type ( (string) $release->country, COUNTRIES, $database, true);
-      $maturity_rating_type_id = get_maturity_rating_id ($release->rating, $database, true);
-      $is_official_release = truthy ($release->offical);
-      $quantities_shipped = (int) $release->shipped;
+    foreach ($cartridge->releases as $release) {
+      echo($release->rating);
+
+      $country_type_id = get_type ($release->release->country, COUNTRIES, $database, true);
+      $maturity_rating_type_id = get_maturity_rating_id ($release->release->rating, $database, true);
+      $is_official_release = truthy ($release->release->offical);
+      $quantities_shipped = $release->release->shipped;
+      $release_date = $release->release->release_date;
 
       $last_release_id = $database->insert (TBL_RELEASE, [
         'cartridge_id' => (int) $last_cartridge_id,
         'country_type_id' => (int) $country_type_id,
         'maturity_rating_type_id' => (int) $maturity_rating_type_id,
+        'release_date' => (string) $release_date,
         'is_official_release' => (int) $is_official_release,
-        'release_date' => (string) $release->release_date,
         'quantities_shipped' => (int) $quantities_shipped,
       ]);
+      check_database_error($database);
 
-      foreach ($release->boxart as $media) {
-        $last_media_id = $database->insert ('media', [
-          'foreign_id ' => (int) $last_release_id,
-          'foreign_type ' =>  $media['foreign_type'],
-          'file_name ' => (string) $media,
-          'role ' => $media['role'],
-          'mime_type ' => $media['mime'],
-        ]);
-        check_database_error($database);
-      }
+      // Associating Boaxart
+      // boaxart are processed after cartridge insert
+      // TODO: Box Art / Database design for it.
+      // foreach ($release->release->boxart as $media) {
+      //   $file_name = $media->media;
+      //
+      //   $last_media_id = $database->insert (TBL_MEDIA, [
+      //     'foreign_id ' => (int) $last_release_id,
+      //     'foreign_type ' =>  (string) $media['foreign_type'],
+      //     'file_name ' => (string) $file_name,
+      //     'role ' => $media['role'],
+      //     'mime_type ' => $media['mime'],
+      //     'description ' => $media['foreign_type'] . '-' . $media['role'] . '-' . $file_name,
+      //   ]);
+      //   check_database_error($database);
+      // }
     }
   }
 }
