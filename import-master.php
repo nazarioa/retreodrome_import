cf388d2cf9df0cfcd498c65d0bcd1b14a41ba8c8
Nazario A. Ayala <nazario@niztech.com>
2017-09-30 13:32:23 -0700

    LINT

--- import-master.php
+++ import-master.php
@@ -1,18 +1,20 @@
 <?php
+
 namespace Retrodrome\Import;
+
 use Medoo\Medoo;
 
 require_once __DIR__ . '/sensitive-settings.php';
 require_once __DIR__ . '/common.php';
 require_once __DIR__ . '/Medoo/src/Medoo.php';
 
-define('DEBUG', true);
+define('DEBUG', TRUE);
 
 try {
   $database = new Medoo($settings);
 } catch (Exception $e) {
-  $message = 'I cannot connect to the database!' ."\n" . '- Is MySQL running?' ."\n" . '- Do you have the right username and password?' ."\n" . '- Does a database named "'.$settings['database_name'].'"  exist?' . "\n\n" . 'Connection settings are:' . "\n\n";
-  $message .= print_r($settings, true);
+  $message = 'I cannot connect to the database!' . "\n" . '- Is MySQL running?' . "\n" . '- Do you have the right username and password?' . "\n" . '- Does a database named "' . $settings['database_name'] . '"  exist?' . "\n\n" . 'Connection settings are:' . "\n\n";
+  $message .= print_r($settings, TRUE);
 
   die($message);
 }
@@ -20,18 +22,18 @@ try {
 
 echo 'Starting' . "\n\n";
 
-$file_path = 'data/-master-list.xml';
-$file_handle = fopen( join('/', array(__DIR__, $file_path)), 'r');
-$file_data = fread($file_handle, filesize( join('/', array(__DIR__, $file_path))));
+$file_path   = 'data/-master-list.xml';
+$file_handle = fopen(join('/', array(__DIR__, $file_path)), 'r');
+$file_data   = fread($file_handle, filesize(join('/', array(__DIR__, $file_path))));
 
 $xml_data = simplexml_load_string($file_data);
 
 foreach ($xml_data as $system) {
   if (in_array($system['shortcode'], $systems_to_import)) {
-    $source_path = join('/', array($media_path['source'], $system['shortcode']));
+    $source_path      = join('/', array($media_path['source'], $system['shortcode']));
     $destination_path = $media_path['destination'];
 
-    if (IMPORT_MEDIA == true) {
+    if (IMPORT_MEDIA == TRUE) {
       if (
         !isset($destination_path)
         || !is_writable($destination_path)
@@ -40,8 +42,8 @@ foreach ($xml_data as $system) {
       }
 
 
-      if ( !isset($source_path) ) {
-        throw new Exception('Array `media_path["source"]` is not found or writable: ' .$source_path);
+      if (!isset($source_path)) {
+        throw new Exception('Array `media_path["source"]` is not found or writable: ' . $source_path);
       }
     }
 
@@ -51,7 +53,7 @@ foreach ($xml_data as $system) {
       // game exists already since the database is empty.
       $database->insert(TBL_GAME, [
         'description' => (string) $game->cartridge->title,
-        'disabled' => NO,
+        'disabled'    => NO,
       ]);
       check_database_error($database);
       $last_game_id = $database->id();
@@ -59,34 +61,34 @@ foreach ($xml_data as $system) {
 
       // Associating Genres with current game.
       foreach ($game->genres as $genre) {
-        $last_genre_id = get_genre_id((string) $genre->genre, $database, true);
+        $last_genre_id = get_genre_id((string) $genre->genre, $database, TRUE);
         $database->insert(TBL_GAME_GENRE, [
           'genre_type_id' => $last_genre_id,
-          'game_id' => $last_game_id,
+          'game_id'       => $last_game_id,
         ]);
         check_database_error($database);
       }
 
       // Associating "Cartridges" with current game.
       foreach ($game->cartridge as $cartridge) {
-        $title = (string) $cartridge->title;
-        $region_type_id = get_region_id((string) $cartridge->region, $database, true);
-        $game_default = (!empty($cartridge['primary']) && 'Y' == $cartridge['primary']) ?  YES : NO;
-        $license = truthy($cartridge->license);
-        $demo = truthy($cartridge->demo);
-        $prototype = truthy($cartridge->prototype);
-        $special = truthy($cartridge->special);
+        $title          = (string) $cartridge->title;
+        $region_type_id = get_region_id((string) $cartridge->region, $database, TRUE);
+        $game_default   = (!empty($cartridge['primary']) && 'Y' == $cartridge['primary']) ? YES : NO;
+        $license        = truthy($cartridge->license);
+        $demo           = truthy($cartridge->demo);
+        $prototype      = truthy($cartridge->prototype);
+        $special        = truthy($cartridge->special);
 
         $database->insert(TBL_CARTRIDGE, [
-          'game_id' => (int) $last_game_id,
+          'game_id'        => (int) $last_game_id,
           'region_type_id' => (int) $region_type_id,
-          'name' => (string) $title,
-          'game_default' => (int) $game_default,
-          'disabled' => NO,
-          'license' => (int) $license,
-          'demo' => (int) $demo,
-          'prototype' => (int) $prototype,
-          'special' => (int) $special,
+          'name'           => (string) $title,
+          'game_default'   => (int) $game_default,
+          'disabled'       => NO,
+          'license'        => (int) $license,
+          'demo'           => (int) $demo,
+          'prototype'      => (int) $prototype,
+          'special'        => (int) $special,
         ]);
         check_database_error($database);
         $last_cartridge_id = $database->id();
@@ -94,9 +96,9 @@ foreach ($xml_data as $system) {
         // Associating Consoles
         // consoles are processed after cartridge insert
         foreach ($cartridge->consoles as $console) {
-          $console_id = get_console_id($console->console, $database, true);
+          $console_id = get_console_id($console->console, $database, TRUE);
           $database->insert(TBL_CARTRIDGE_CONSOLE, [
-            'console_id' => (int) $console_id,
+            'console_id'   => (int) $console_id,
             'cartridge_id' => (int) $last_cartridge_id,
           ]);
           check_database_error($database);
@@ -105,12 +107,12 @@ foreach ($xml_data as $system) {
         // Associating Companies
         // companies are processed after cartridge insert
         foreach ($cartridge->companies as $company) {
-          $company_role_type_id = get_type((string) $company->company['type'], CREATION_ROLE, $database, false);
+          $company_role_type_id = get_type((string) $company->company['type'], CREATION_ROLE, $database, FALSE);
           $company_role_type_id = !empty($company_role_type_id) ? $company_role_type_id : DEFAULT_TYPE;
-          $company_id = get_company_id($company->company, $database, true);
+          $company_id           = get_company_id($company->company, $database, TRUE);
           $database->insert(TBL_CARTRIDGE_COMPANY, [
-            'company_id' => (int) $company_id,
-            'cartridge_id' => (int) $last_cartridge_id,
+            'company_id'           => (int) $company_id,
+            'cartridge_id'         => (int) $last_cartridge_id,
             'company_role_type_id' => (int) $company_role_type_id,
           ]);
           check_database_error($database);
@@ -121,25 +123,26 @@ foreach ($xml_data as $system) {
         foreach ($cartridge->releases as $release) {
 
           $country_type_id = (string) $release->release->country;
-          if(!empty($country_type_id)) {
-            $country_type_id = get_type((string) $release->release->country, COUNTRIES, $database, true);
-          } else {
+          if (!empty($country_type_id)) {
+            $country_type_id = get_type((string) $release->release->country, COUNTRIES, $database, TRUE);
+          }
+          else {
             $country_type_id = get_type(ALL_COUNTRY, COUNTRIES, $database, FALSE);
           }
 
-          $maturity_rating_type_id = get_maturity_rating_id($release->release->rating, $database, false);
+          $maturity_rating_type_id = get_maturity_rating_id($release->release->rating, $database, FALSE);
           $maturity_rating_type_id = !empty($maturity_rating_type_id) ? $maturity_rating_type_id : DEFAULT_TYPE;
-          $is_official_release = truthy($release->release->offical);
-          $quantities_shipped = $release->release->shipped;
-          $release_date = $release->release->release_date;
+          $is_official_release     = truthy($release->release->offical);
+          $quantities_shipped      = $release->release->shipped;
+          $release_date            = $release->release->release_date;
 
           $database->insert(TBL_RELEASE, [
-            'cartridge_id' => (int) $last_cartridge_id,
-            'country_type_id' => (int) $country_type_id,
+            'cartridge_id'            => (int) $last_cartridge_id,
+            'country_type_id'         => (int) $country_type_id,
             'maturity_rating_type_id' => (int) $maturity_rating_type_id,
-            'release_date' => (string) $release_date,
-            'is_official_release' => (int) $is_official_release,
-            'quantities_shipped' => (int) $quantities_shipped,
+            'release_date'            => (string) $release_date,
+            'is_official_release'     => (int) $is_official_release,
+            'quantities_shipped'      => (int) $quantities_shipped,
           ]);
           check_database_error($database);
           $last_release_id = $database->id();
@@ -147,28 +150,33 @@ foreach ($xml_data as $system) {
           // Associating Boaxart
           // boaxart are processed after cartridge insert
           foreach ($release->release->boxart->media as $media) {
-            $file_name = (string) $media;
+            $file_name    = (string) $media;
             $foreign_type = (string) $media['foreign_type'];
-            $role = (string) $media['role'];
-            $mime_type = (string) $media['mime'];
-            $description = $foreign_type . '-' . $role . '-' . $file_name;
+            $role         = (string) $media['role'];
+            $mime_type    = (string) $media['mime'];
+            $description  = $foreign_type . '-' . $role . '-' . $file_name;
 
             $database->insert(TBL_MEDIA, [
-              'foreign_id' => $last_release_id,
+              'foreign_id'   => $last_release_id,
               'foreign_type' => $foreign_type,
-              'file_name' => $file_name,
-              'role' => $role,
-              'mime_type' => $mime_type,
-              'description' => $description,
+              'file_name'    => $file_name,
+              'role'         => $role,
+              'mime_type'    => $mime_type,
+              'description'  => $description,
             ]);
             check_database_error($database);
             $last_media_id = $database->id();
 
-            if (IMPORT_MEDIA == true) {
+            if (IMPORT_MEDIA == TRUE) {
               $full_source_path = join('/', array($source_path, $file_name));
 
-              $full_destination_folder = implode('/', [$media_path['destination'], 'game', 'release', $last_release_id]);
-              if(!file_exists($full_destination_folder)) {
+              $full_destination_folder = implode('/', [
+                $media_path['destination'],
+                'game',
+                'release',
+                $last_release_id
+              ]);
+              if (!file_exists($full_destination_folder)) {
                 $r = mkdir($full_destination_folder, 0766, TRUE);
               }
               $full_destination_path = implode('/', [$full_destination_folder, $file_name]);
