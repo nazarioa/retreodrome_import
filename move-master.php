3c099403515c213a19c2c462318abf2d9256ac17
Nazario A. Ayala <nazario@niztech.com>
2017-09-30 13:24:26 -0700

    LINT

--- move-master.php
+++ move-master.php
@@ -20,27 +20,14 @@ require_once __DIR__ . '/Medoo/medoo.php';
  * Tests the database connection.
  */
 try {
-    $database = new medoo($settings);
+  $database = new medoo($settings);
 } catch (Exception $e) {
-    $message = 'I cannot connect to the database!' . "\n" . '- Is MySQL running?' . "\n" . '- Do you have the right username and password?' . "\n" . '- Does a database named "' . $settings['database_name'] . '"  exist?' . "\n\n" . 'Connection settings are:' . "\n\n";
-    $message .= print_r($settings, true);
+  $message = 'I cannot connect to the database!' . "\n" . '- Is MySQL running?' . "\n" . '- Do you have the right username and password?' . "\n" . '- Does a database named "' . $settings['database_name'] . '"  exist?' . "\n\n" . 'Connection settings are:' . "\n\n";
+  $message .= print_r($settings, TRUE);
 
-    die($message);
+  die($message);
 }
 
-if (IMPORT_MEDIA == true) {
-    if (
-    !isset($media_path['destination'])
-    || !is_writable($media_path['destination'])
-  ) {
-        throw new Exception('Array `media_path["destination"]` is not found or writable: ' . $media_path['destination']);
-    }
-
-    if (
-  !isset($media_path['source'])
-  ) {
-        throw new Exception('Array `media_path["source"]` is not found or writable: ' . $media_path['source']);
-    }
 /*
  * This is a pref-light check.
  * If the IMPORT_MEDIA flag is on, lets make sure we have the correct environment variables.
@@ -50,14 +37,23 @@ if (IMPORT_MEDIA == true) {
  *
  * @param $media_path['source'] : Place where the raw media is coming from.
  */
+if (IMPORT_MEDIA == TRUE) {
+  if (!isset($media_path['destination'])
+      || !is_writable($media_path['destination'])) {
+    throw new Exception('Array `media_path["destination"]` is not found or writable: ' . $media_path['destination']);
+  }
+
+  if (!isset($media_path['source'])) {
+    throw new Exception('Array `media_path["source"]` is not found or writable: ' . $media_path['source']);
+  }
 }
 
 echo 'Starting' . "\n\n";
 
-$data = $database->select(TBL_MEDIA, [
 /*
  * Gets all the artwork information that currently lives in the database.
  */
+$mediaData = $database->select(TBL_MEDIA, [
   'foreign_id',
   'foreign_type',
   'file_name',
@@ -70,19 +66,26 @@ check_database_error($database);
 /*
  * For each entry in the media table:
  */
+foreach ($mediaData as $key => $mediaEntry) {
+  /*
+   * Calculate the source path where we expect the file to be.
+   * Calculate the destination path where we want the file to endup.
+   */
+  $full_source_path      = implode('/', [$media_path['source'], $mediaEntry['file_name']]);
+  $full_destination_path = implode('/', [
+    $media_path['destination'],
+    'game',
+    $item['foreign_type'],
+    $item['foreign_id'],
+  ]);
 
-
-foreach ($data as $key => $item) {
-    $full_source_path      = implode('/', [$media_path['source'], $item['file_name']]);
-    $full_destination_path = implode('/', [$media_path['destination'], 'game',  $item['foreign_type'], $item['foreign_id'],]);
-
-    if (file_exists($full_source_path)) {
-        mkdir($full_destination_path);
-        rename($full_source_path, $full_destination_path .  '/' . $item['file_name']);
-    } else {
-        echo $item['file_name'] . PHP_EOL;
-    }
   /*
    * If the file exists, move the file to its new home.
    */
+  if (file_exists($full_source_path)) {
+    mkdir($full_destination_path);
+    rename($full_source_path, $full_destination_path . '/' . $item['file_name']);
+  } else {
+    echo $item['file_name'] . PHP_EOL;
+  }
 }
