7f380c684edeebf01049a1ab3d82a957cb691225
Nazario A. Ayala <nazario@niztech.com>
2017-09-30 13:23:47 -0700

    Added comments to move file.

--- move-master.php
+++ move-master.php
@@ -4,11 +4,21 @@
  * User: nazario
  * Date: 3/4/17
  * Time: 9:52 PM
+ *
+ * This script is used to move image artwork from an unorganized file system
+ * to its correct spot in webserver. The correct spot correlates with and entry
+ * in the database.
+ * This script assumes that the entry in the database already exists and wont
+ * create new entries in the database.
  */
 require_once __DIR__ . '/sensitive-settings.php';
 require_once __DIR__ . '/common.php';
 require_once __DIR__ . '/Medoo/medoo.php';
 
+/*
+ * This is a pref-light check.
+ * Tests the database connection.
+ */
 try {
     $database = new medoo($settings);
 } catch (Exception $e) {
@@ -31,11 +41,23 @@ if (IMPORT_MEDIA == true) {
   ) {
         throw new Exception('Array `media_path["source"]` is not found or writable: ' . $media_path['source']);
     }
+/*
+ * This is a pref-light check.
+ * If the IMPORT_MEDIA flag is on, lets make sure we have the correct environment variables.
+ *
+ * @param $media_path['destination'] : Root directory of cakePHPs file storage
+ * defined as webroot/files/
+ *
+ * @param $media_path['source'] : Place where the raw media is coming from.
+ */
 }
 
 echo 'Starting' . "\n\n";
 
 $data = $database->select(TBL_MEDIA, [
+/*
+ * Gets all the artwork information that currently lives in the database.
+ */
   'foreign_id',
   'foreign_type',
   'file_name',
@@ -45,6 +67,9 @@ $data = $database->select(TBL_MEDIA, [
 
 check_database_error($database);
 
+/*
+ * For each entry in the media table:
+ */
 
 
 foreach ($data as $key => $item) {
@@ -57,4 +82,7 @@ foreach ($data as $key => $item) {
     } else {
         echo $item['file_name'] . PHP_EOL;
     }
+  /*
+   * If the file exists, move the file to its new home.
+   */
 }
