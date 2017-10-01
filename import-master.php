6f020647230c6cef069a0bb904cc37ac069031ce
Martin Maldonado <martin.maldonado7@gmail.com>
2017-09-30 22:48:07 -0700

    Added logic in to only import cartridges with region type NTSC

--- import-master.php
+++ import-master.php
@@ -101,116 +101,120 @@ foreach ($xml_data as $system) {
 
       // Associating "Cartridges" with current game.
       foreach ($game->cartridge as $cartridge) {
-        $title          = (string) $cartridge->title;
-        $region_type_id = get_region_id((string) $cartridge->region, $database, TRUE);
-        $game_default   = (!empty($cartridge['primary']) && 'Y' == $cartridge['primary']) ? YES : NO;
-        $license        = truthy($cartridge->license);
-        $demo           = truthy($cartridge->demo);
-        $prototype      = truthy($cartridge->prototype);
-        $special        = truthy($cartridge->special);
 
-        $database->insert(TBL_CARTRIDGE, [
-          'game_id'        => (int) $last_game_id,
-          'region_type_id' => (int) $region_type_id,
-          'name'           => (string) $title,
-          'game_default'   => (int) $game_default,
-          'disabled'       => NO,
-          'license'        => (int) $license,
-          'demo'           => (int) $demo,
-          'prototype'      => (int) $prototype,
-          'special'        => (int) $special,
-        ]);
-        check_database_error($database);
-        $last_cartridge_id = $database->id();
+        if ($cartridge->region == 'NTSC'){
 
-        // Associating Consoles
-        // consoles are processed after cartridge insert
-        foreach ($cartridge->consoles as $console) {
-          $console_id = get_console_id($console->console, $database, TRUE);
-          $database->insert(TBL_CARTRIDGE_CONSOLE, [
-            'console_id'   => (int) $console_id,
-            'cartridge_id' => (int) $last_cartridge_id,
+          $title          = (string) $cartridge->title;
+          $region_type_id = get_region_id((string) $cartridge->region, $database, TRUE);
+          $game_default   = (!empty($cartridge['primary']) && 'Y' == $cartridge['primary']) ? YES : NO;
+          $license        = truthy($cartridge->license);
+          $demo           = truthy($cartridge->demo);
+          $prototype      = truthy($cartridge->prototype);
+          $special        = truthy($cartridge->special);
+
+          $database->insert(TBL_CARTRIDGE, [
+            'game_id'        => (int) $last_game_id,
+            'region_type_id' => (int) $region_type_id,
+            'name'           => (string) $title,
+            'game_default'   => (int) $game_default,
+            'disabled'       => NO,
+            'license'        => (int) $license,
+            'demo'           => (int) $demo,
+            'prototype'      => (int) $prototype,
+            'special'        => (int) $special,
           ]);
           check_database_error($database);
-        }
+          $last_cartridge_id = $database->id();
 
-        // Associating Companies
-        // companies are processed after cartridge insert
-        foreach ($cartridge->companies as $company) {
-          $company_role_type_id = get_type((string) $company->company['type'], CREATION_ROLE, $database, FALSE);
-          $company_role_type_id = !empty($company_role_type_id) ? $company_role_type_id : DEFAULT_TYPE;
-          $company_id           = get_company_id($company->company, $database, TRUE);
-          $database->insert(TBL_CARTRIDGE_COMPANY, [
-            'company_id'           => (int) $company_id,
-            'cartridge_id'         => (int) $last_cartridge_id,
-            'company_role_type_id' => (int) $company_role_type_id,
-          ]);
-          check_database_error($database);
-        }
-
-        // Associating Releases
-        // releases are processed after cartridge insert
-        foreach ($cartridge->releases as $release) {
-
-          $country_type_id = (string) $release->release->country;
-          if (!empty($country_type_id)) {
-            $country_type_id = get_type((string) $release->release->country, COUNTRIES, $database, TRUE);
-          }
-          else {
-            $country_type_id = get_type(ALL_COUNTRY, COUNTRIES, $database, FALSE);
-          }
-
-          $maturity_rating_type_id = get_maturity_rating_id($release->release->rating, $database, FALSE);
-          $maturity_rating_type_id = !empty($maturity_rating_type_id) ? $maturity_rating_type_id : DEFAULT_TYPE;
-          $is_official_release     = truthy($release->release->offical);
-          $quantities_shipped      = $release->release->shipped;
-          $release_date            = $release->release->release_date;
-
-          $database->insert(TBL_RELEASE, [
-            'cartridge_id'            => (int) $last_cartridge_id,
-            'country_type_id'         => (int) $country_type_id,
-            'maturity_rating_type_id' => (int) $maturity_rating_type_id,
-            'release_date'            => (string) $release_date,
-            'is_official_release'     => (int) $is_official_release,
-            'quantities_shipped'      => (int) $quantities_shipped,
-          ]);
-          check_database_error($database);
-          $last_release_id = $database->id();
-
-          // Associating Boaxart
-          // boaxart are processed after cartridge insert
-          foreach ($release->release->boxart->media as $media) {
-            $file_name    = (string) $media;
-            $foreign_type = (string) $media['foreign_type'];
-            $role         = (string) $media['role'];
-            $mime_type    = (string) $media['mime'];
-            $description  = $foreign_type . '-' . $role . '-' . $file_name;
-
-            $database->insert(TBL_MEDIA, [
-              'foreign_id'   => $last_release_id,
-              'foreign_type' => $foreign_type,
-              'file_name'    => $file_name,
-              'role'         => $role,
-              'mime_type'    => $mime_type,
-              'description'  => $description,
+          // Associating Consoles
+          // consoles are processed after cartridge insert
+          foreach ($cartridge->consoles as $console) {
+            $console_id = get_console_id($console->console, $database, TRUE);
+            $database->insert(TBL_CARTRIDGE_CONSOLE, [
+              'console_id'   => (int) $console_id,
+              'cartridge_id' => (int) $last_cartridge_id,
             ]);
             check_database_error($database);
-            $last_media_id = $database->id();
+          }
 
-            if (IMPORT_MEDIA == TRUE) {
-              $full_source_path = join('/', array($source_path, $file_name));
+          // Associating Companies
+          // companies are processed after cartridge insert
+          foreach ($cartridge->companies as $company) {
+            $company_role_type_id = get_type((string) $company->company['type'], CREATION_ROLE, $database, FALSE);
+            $company_role_type_id = !empty($company_role_type_id) ? $company_role_type_id : DEFAULT_TYPE;
+            $company_id           = get_company_id($company->company, $database, TRUE);
+            $database->insert(TBL_CARTRIDGE_COMPANY, [
+              'company_id'           => (int) $company_id,
+              'cartridge_id'         => (int) $last_cartridge_id,
+              'company_role_type_id' => (int) $company_role_type_id,
+            ]);
+            check_database_error($database);
+          }
 
-              $full_destination_folder = implode('/', [
-                $media_path['destination'],
-                'game',
-                'release',
-                $last_release_id
+          // Associating Releases
+          // releases are processed after cartridge insert
+          foreach ($cartridge->releases as $release) {
+
+            $country_type_id = (string) $release->release->country;
+            if (!empty($country_type_id)) {
+              $country_type_id = get_type((string) $release->release->country, COUNTRIES, $database, TRUE);
+            }
+            else {
+              $country_type_id = get_type(ALL_COUNTRY, COUNTRIES, $database, FALSE);
+            }
+
+            $maturity_rating_type_id = get_maturity_rating_id($release->release->rating, $database, FALSE);
+            $maturity_rating_type_id = !empty($maturity_rating_type_id) ? $maturity_rating_type_id : DEFAULT_TYPE;
+            $is_official_release     = truthy($release->release->offical);
+            $quantities_shipped      = $release->release->shipped;
+            $release_date            = $release->release->release_date;
+
+            $database->insert(TBL_RELEASE, [
+              'cartridge_id'            => (int) $last_cartridge_id,
+              'country_type_id'         => (int) $country_type_id,
+              'maturity_rating_type_id' => (int) $maturity_rating_type_id,
+              'release_date'            => (string) $release_date,
+              'is_official_release'     => (int) $is_official_release,
+              'quantities_shipped'      => (int) $quantities_shipped,
+            ]);
+            check_database_error($database);
+            $last_release_id = $database->id();
+
+            // Associating Boaxart
+            // boaxart are processed after cartridge insert
+            foreach ($release->release->boxart->media as $media) {
+              $file_name    = (string) $media;
+              $foreign_type = (string) $media['foreign_type'];
+              $role         = (string) $media['role'];
+              $mime_type    = (string) $media['mime'];
+              $description  = $foreign_type . '-' . $role . '-' . $file_name;
+
+              $database->insert(TBL_MEDIA, [
+                'foreign_id'   => $last_release_id,
+                'foreign_type' => $foreign_type,
+                'file_name'    => $file_name,
+                'role'         => $role,
+                'mime_type'    => $mime_type,
+                'description'  => $description,
               ]);
-              if (!file_exists($full_destination_folder)) {
-                $r = mkdir($full_destination_folder, 0766, TRUE);
+              check_database_error($database);
+              $last_media_id = $database->id();
+
+              if (IMPORT_MEDIA == TRUE) {
+                $full_source_path = join('/', array($source_path, $file_name));
+
+                $full_destination_folder = implode('/', [
+                  $media_path['destination'],
+                  'game',
+                  'release',
+                  $last_release_id
+                ]);
+                if (!file_exists($full_destination_folder)) {
+                  $r = mkdir($full_destination_folder, 0766, TRUE);
+                }
+                $full_destination_path = implode('/', [$full_destination_folder, $file_name]);
+                rename($full_source_path, $full_destination_path);
               }
-              $full_destination_path = implode('/', [$full_destination_folder, $file_name]);
-              rename($full_source_path, $full_destination_path);
             }
           }
         }
